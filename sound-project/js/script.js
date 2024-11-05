dfc8443814c604519ca65b897421699f8efbdbfa
Laine294 <chinguunmagnai@gmail.com>
2024-11-05 13:35:18 +0800

    nnn

---
+++ sound-project/js/script.js
@@ -0,0 +1,28 @@
+function setup() {
+    let canvas = createCanvas(500, 400);
+    canvas.parent("p5-canvas-container");
+}
+
+let sound;
+
+function preload() {
+    sound = loadSound("assets/song.mp3");
+}
+
+function draw() {
+    background(0);
+    // let vol = map(mouseY, 0, height, 0.0, 1.0, true);
+    // sound.setVolume(vol);
+
+    let panVal = map(mouseX, 0, width, 1.0, -1.0, true);
+    sound.pan(panVal);
+
+    let rateVal = map(mouseY, 0, height, 0.1, 1.5, true);
+    sound.rate(rateVal);
+}
+
+function mouseClicked() {
+    if (sound.isPlaying() == false) {
+        sound.play();
+    }
+}
