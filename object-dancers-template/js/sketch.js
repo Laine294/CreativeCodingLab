eceb8bf3e160c2ac8e1fc15a13fc84c89f64ded7
Laine294 <chinguunmagnai@gmail.com>
2024-11-05 14:32:12 +0800

    Update sketch.js

--- object-dancers-template/js/sketch.js
+++ object-dancers-template/js/sketch.js
@@ -18,7 +18,7 @@ function setup() {
   canvas.parent("p5-canvas-container");
 
   // ...except to adjust the dancer's name on the next line:
-  dancer = new MediocreDancer(width / 2, height / 2);
+  dancer = MediocreDancer(width / 2, height / 2);
 }
 
 function draw() {
