fb05969ad43c2b3731b2c720d9006e8086ba712f
Laine294 <chinguunmagnai@gmail.com>
2024-11-10 22:41:59 +0800

    Dynamic

--- object-dancers-template/js/sketch.js
+++ object-dancers-template/js/sketch.js
@@ -13,25 +13,21 @@
 let dancer;
 
 function setup() {
-  // no adjustments in the setup function needed...
+
   let canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("p5-canvas-container");
-
-  // ...except to adjust the dancer's name on the next line:
   dancer = new MediocreDancer(width / 2, height / 2);
 }
 
 function draw() {
-  // you don't need to make any adjustments inside the draw loop
+
   background(0);
-  drawFloor(); // for reference only
+  drawFloor();
 
   dancer.update();
   dancer.display();
 }
 
-// You only code inside this class.
-// Start by giving the dancer your name, e.g. LeonDancer.
 class MediocreDancer {
   constructor(startX, startY) {
     this.x = startX;
@@ -61,9 +57,6 @@ class MediocreDancer {
     push();
     translate(this.x, this.y);
     rotate(this.angle);
-
-    // ******** //
-    // ⬇️ draw your dancer from here ⬇️
     fill(this.color);
     ellipse(0, 0, this.size * 0.5, this.size * 0.7);
     fill(255);
@@ -73,51 +66,6 @@ class MediocreDancer {
     line(-this.leglength * 0.5, this.size * 0.3, -this.leglength, this.size);
     line(this.leglength * 0.5, this.size * 0.3, this.leglength, this.size);
 
-
-
-
-
-
-
-
-    // ⬆️ draw your dancer above ⬆️
-    // ******** //
-
-    // the next function draws a SQUARE and CROSS
-    // to indicate the approximate size and the center point
-    // of your dancer.
-    // it is using "this" because this function, too, 
-    // is a part if your Dancer object.
-    // comment it out or delete it eventually.
-    // this.drawReferenceShapes()
-
     pop();
   }
-  // drawReferenceShapes() {
-  //   noFill();
-  //   stroke(255, 0, 0);
-  //   line(-5, 0, 5, 0);
-  //   line(0, -5, 0, 5);
-  //   stroke(255);
-  //   rect(-100, -100, 200, 200);
-  //   fill(255);
-  //   stroke(0);
-  // }
 }
-
-
-
-/*
-GOAL:
-The goal is for you to write a class that produces a dancing being/creature/object/thing. In the next class, your dancer along with your peers' dancers will all dance in the same sketch that your instructor will put together. 
-
-RULES:
-For this to work you need to follow one rule: 
-  - Only put relevant code into your dancer class; your dancer cannot depend on code outside of itself (like global variables or functions defined outside)
-  - Your dancer must perform by means of the two essential methods: update and display. Don't add more methods that require to be called from outside (e.g. in the draw loop).
-  - Your dancer will always be initialized receiving two arguments: 
-    - startX (currently the horizontal center of the canvas)
-    - startY (currently the vertical center of the canvas)
-  beside these, please don't add more parameters into the constructor function 
-  - lastly, to make sure our dancers will harmonize once on the same canvas, please don't make your dancer bigger than 200x200 pixels. 
-*/
